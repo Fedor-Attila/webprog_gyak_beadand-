8b45c455edb68e17cb019ece48b182f218517f6f
Attila Fedor <66425590+attila-fedor@users.noreply.github.com>
2025-05-03 22:14:42 +0200

    elso commit

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+// Weboldal címe
+$siteTitle = "Webprog Beadandó Projekt";
+
+// Menüpontok tömbje: 'kulcs' => 'Megjelenítendő név'
+$menuItems = [
+    'fooldal'    => 'Főoldal',
+    'kepek'      => 'Képek',
+    'kapcsolat'  => 'Kapcsolat',
+    'uzenetek'   => 'Üzenetek',
+    'belepes'    => 'Belépés',
+    'kilepes'    => 'Kilépés'
+];
+
+// Adatbázis kapcsolat beállításai
+$db_config = [
+    'host'     => 'localhost',
+    'dbname'   => 'ccp9c6',
+    'user'     => 'ccp9c6',
+    'password' => '19910226' // FIGYELEM: csak helyi fejlesztéshez, publikáláskor ezt cseréld!
+];
+?>
